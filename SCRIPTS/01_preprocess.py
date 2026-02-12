5b9d41ade957d5c9a7cf6a5f76a322ac7964929f
siwenliao <siwenliao105@gmail.com>
2026-02-12 22:49:19 +0000

    Update preprocessing script and cleaned dataset

--- SCRIPTS/01_preprocess.py
+++ SCRIPTS/01_preprocess.py
@@ -6,14 +6,15 @@ import string
 import nltk
 from nltk.corpus import stopwords
 import en_core_web_md
+import re
 nltk.download('wordnet')
 nltk.download('stopwords')
 
-df = pd.read_csv('./DATA/McDonalds_Reviews.csv', encoding="latin1")
+df = pd.read_csv('/content/DS-4002_Group5_Project1/DATA/McDonalds_Reviews.csv', encoding="latin1")
 nlp = en_core_web_md.load(disable=['parser', 'ner'])
 
 # Subsetting our data to only variables of interest
-df = df[['review','rating']] 
+df = df[['review','rating']]
 
 # Understanding the data
 print(df.head(), '\n')
@@ -21,27 +22,29 @@ print(df.tail(), '\n')
 print('Number of reviews:', len(df), '\n')
 print('Checking for missing values: \n', df.isnull().sum(), '\n')
 
-# An extensive list of English stopwords 
+# An extensive list of English stopwords
  # (commonly-used, high-frequency words that carry little semantic meaning on their own)
 stop_words = set(stopwords.words('english'))
 
-# Cleaning text 
+# Cleaning text
 def clean_text(text):
 
   # Lowercase
   text = str(text).lower()
-  
+
   # Remove puncuation
   delete_dict = {punc: '' for punc in string.punctuation}
   table = str.maketrans(delete_dict)
   text_nopunc = text.translate(table)
 
+  text_nopunc = re.sub(r"[^a-zA-Z0-9\s]", "", text_nopunc)
+
   # Split into a list (tokens)
   text_arr = text_nopunc.split()
 
   # Remove digits, one-letter words, and stopwords
-  text_keep = ' '.join([w for w in text_arr if (not w.isdigit()) 
-                and (len(w) > 1) 
+  text_keep = ' '.join([w for w in text_arr if (not w.isdigit())
+                and (len(w) > 1)
                 and (w not in stop_words)])
 
   return text_keep
@@ -51,11 +54,11 @@ df['review'] = df['review'].apply(clean_text)
 def lemmatization(text):
     doc = nlp(text)
 
-    # Converts reviews into lemmas (base-form words i.e. help reduce duplicate word forms) 
+    # Converts reviews into lemmas (base-form words i.e. help reduce duplicate word forms)
     # Only keeping nouns and adjectives
     lemmas = [t.lemma_ for t in doc if t.pos_ in {"NOUN", "ADJ"}]
     return ' '.join(lemmas)
 
 df['review'] = df['review'].apply(lemmatization)
 
-df.to_csv('./DATA/McDonalds_Reviews_Cleaned.csv')
+df.to_csv('/content/DS-4002_Group5_Project1/DATA/McDonalds_Reviews_Cleaned.csv')
